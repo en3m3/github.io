20ee2625f2f5f0fb0fb99bf25b9e648e6e97d94d
en3m3 <mof20002@byui.edu>
2020-09-26 12:34:37 -0600

    changed main section to a main html element. Added JS to make the copyright year dynamic, moved the images up into the header. Updated positioning CSS and font-size styling. Removed the second media query that was no longer needed.

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@ var months = ["Janurary", "February", "March", "April", "May", "June", "July", "
 let modified = new Date(document.lastModified);
 let formattedDate = `Last updated: ${months[modified.getMonth()]} ${modified.getDate()}, ${modified.getUTCFullYear()}`;
 document.getElementById("modified").innerHTML = formattedDate;
+
+let today = new Date();
+let theYear = today.getFullYear();
+document.getElementById("year").innerHTML = theYear;
