6936ce332f1a0c88967a2440fb9211952da5284a
en3m3 <mof20002@byui.edu>
2020-09-25 22:37:16 -0600

    updated mobile first css, added js to open and close when clicked. Removed style.css, css was moved to small.css

--- lesson2/js/script.js
+++ lesson2/js/script.js
@@ -1,10 +1,18 @@
 var checkboxes = document.querySelectorAll(".accordian-toggle");
 checkboxes.forEach(item => {
-    item.addEventListener("click",function(){
+    item.addEventListener("click",function(e){
         checkboxes.forEach(item2 => {
-            item2.checked = false;
-        });
-        item.checked = true;
+            if(this != e.path[0]) {
+                item2.checked = false;
+            } else{
+                if(!e.checked) {
+                    e.checked = false;
+                } 
+                if(e.checked) {
+                    e.checked = true;
+                }
+            }
+        })
     })
 });
 
