acdf556c35c0c33e053f59ac3e7568f301dc6e71
en3m3 <mof20002@byui.edu>
2020-09-25 23:03:21 -0600

    fixed the js

--- lesson2/js/script.js
+++ lesson2/js/script.js
@@ -1,18 +1,21 @@
 var checkboxes = document.querySelectorAll(".accordian-toggle");
 checkboxes.forEach(item => {
     item.addEventListener("click",function(e){
-        checkboxes.forEach(item2 => {
-            if(this != e.path[0]) {
-                item2.checked = false;
-            } else{
-                if(!e.checked) {
-                    e.checked = false;
-                } 
-                if(e.checked) {
-                    e.checked = true;
+        checkboxes.forEach(item2 => {            
+            if(item2 === this) {
+                console.log(this.checked)
+                if(this.checked != true) {
+                    this.checked = false;
+                } else {
+                    this.checked = true;
                 }
+            } else {
+                console.log('false')
+                item2.checked = false;
             }
         })
+
+        
     })
 });
 
