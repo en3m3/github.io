8c92fa4b19bfdb04f85820ed69ef0c74ca98189c
en3m3 <mof20002@byui.edu>
2020-09-25 23:05:12 -0600

    removed console logs

--- lesson2/js/script.js
+++ lesson2/js/script.js
@@ -3,14 +3,12 @@ checkboxes.forEach(item => {
     item.addEventListener("click",function(e){
         checkboxes.forEach(item2 => {            
             if(item2 === this) {
-                console.log(this.checked)
                 if(this.checked != true) {
                     this.checked = false;
                 } else {
                     this.checked = true;
                 }
             } else {
-                console.log('false')
                 item2.checked = false;
             }
         })
