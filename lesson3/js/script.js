febe653d451a3589f007858304f2f7a5fbdd23c0
en3m3 <mof20002@byui.edu>
2020-10-03 14:17:34 -0600

    created content for target audience page + styling

--- lesson3/js/script.js
+++ lesson3/js/script.js
@@ -1,19 +1,8 @@
-var checkboxes = document.querySelectorAll(".accordian-toggle");
-checkboxes.forEach(item => {
-    item.addEventListener("click",function(e){
-        checkboxes.forEach(item2 => {            
-            if(item2 === this) {
-                if(this.checked != true) {
-                    this.checked = false;
-                } else {
-                    this.checked = true;
-                }
-            } else {
-                item2.checked = false;
-            }
-        })
+var months = ["Janurary", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November"];
+let modified = new Date(document.lastModified);
+let formattedDate = `Last updated: ${months[modified.getMonth()]} ${modified.getDate()}, ${modified.getUTCFullYear()}`;
+document.getElementById("modified").innerHTML = formattedDate;
 
-        
-    })
-});
-
+let today = new Date();
+let theYear = today.getFullYear();
+document.getElementById("year").innerHTML = theYear;
