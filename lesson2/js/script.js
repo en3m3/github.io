773a8cf6228158ea9699dfe8b3edc3a155d1cf6f
en3m3 <mof20002@byui.edu>
2020-10-07 23:42:20 -0600

    replaced font sizing. Changed menu toggle to js

--- lesson2/js/script.js
+++ lesson2/js/script.js
@@ -1,19 +1,44 @@
-var checkboxes = document.querySelectorAll(".accordian-toggle");
-checkboxes.forEach(item => {
+var toggle = document.querySelectorAll(".accordian-item");
+toggle.forEach(item => {
     item.addEventListener("click",function(e){
-        checkboxes.forEach(item2 => {            
+        toggle.forEach(item2 => {       
             if(item2 === this) {
-                if(this.checked != true) {
-                    this.checked = false;
+                if(this.classList.contains("show")) {
+                    this.classList.remove("show"); 
+                    main.style.height = 100 + "px";  
                 } else {
-                    this.checked = true;
+                    this.classList.add("show"); 
+                    let main = document.getElementById("main");
+                    main.style.height = 1500 + "px";
+
                 }
             } else {
-                item2.checked = false;
+                item2.classList.remove("show");
+                main.style.height = 100 + "px"; 
             }
-        })
-
-        
-    })
+        });    
+    });
 });
 
+var months = ["Janurary", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November"];
+let modified = new Date(document.lastModified);
+let formattedDate = `Last updated: ${months[modified.getMonth()]} ${modified.getDate()}, ${modified.getUTCFullYear()}`;
+document.getElementById("modified").innerHTML = formattedDate;
+
+let today = new Date();
+let theYear = today.getFullYear();
+document.getElementById("year").innerHTML = theYear;
+
+setTimeout(function(){
+    let foot = document.getElementsByTagName("footer")[0];
+    foot.classList.add("hide");
+
+    document.addEventListener("mousemove", function(e){
+        let foot = document.getElementsByTagName("footer")[0];
+        if(e.clientY>750) {
+            foot.classList.remove("hide");
+        } else {
+            foot.classList.add("hide");
+        }
+    });        
+},1500);
