beef3cdd092e3e573bdb4debfbc2cf9b8937a8d3
en3m3 <mof20002@byui.edu>
2020-10-22 15:38:40 -0600

    lesson 6 starting point

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,35 +1,8 @@
 const navtoggle = document.querySelector('#mobile-toggle');
 const nav = document.querySelector('nav');
-const animDuration = 900;
-
 
 function toggleNav() {
-    navtoggle.removeEventListener("click", toggleNav);
-    if (nav.classList.contains("nav-open")) {
-        closeNav();
-    } else {
-        openNav();
-    }
-}
-
-function openNav() {
-    window.removeEventListener("resize", closeNav);
-    nav.classList.add("nav-open");
-    nav.classList.add("absolute");
-    setTimeout(function () {
-        navtoggle.addEventListener("click", toggleNav);
-        window.addEventListener("resize", closeNav);
-    }, animDuration);
-}
-
-function closeNav() {
-    window.removeEventListener("resize", closeNav);
-    nav.classList.remove("nav-open");
-    setTimeout(function () {
-        nav.classList.remove("absolute");
-        navtoggle.addEventListener("click", toggleNav);
-        window.addEventListener("resize", closeNav);
-    }, animDuration);
+    nav.classList.toggle("nav-open");
 }
 
 var months = ["Janurary", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November"];
@@ -43,4 +16,3 @@ document.getElementById("year").innerHTML = theYear;
 
 
 navtoggle.addEventListener("click", toggleNav);
-window.addEventListener("resize", closeNav);
