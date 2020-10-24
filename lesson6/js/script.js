a75692001f4473acd60018bba55243285a97a0f8
en3m3 <mof20002@byui.edu>
2020-10-23 18:59:27 -0600

    updated changes to lesson 4 to 5 and 6

--- lesson6/js/script.js
+++ lesson6/js/script.js
@@ -1,6 +1,5 @@
 const navtoggle = document.querySelector('#mobile-toggle');
 const nav = document.querySelector('nav');
-const animDuration = 900;
 
 var months = ["Janurary", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November"];
 let modified = new Date();
@@ -8,7 +7,7 @@ let formattedDate = `${months[modified.getMonth()]} ${modified.getDate()}, ${mod
 document.getElementById("modified").innerHTML = formattedDate;
 
 navtoggle.addEventListener("click", function() {
-    nav.toggle("nav-open");
+    nav.classList.toggle("nav-open");
 });
 
 document.addEventListener("DOMContentLoaded", function(){
