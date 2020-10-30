f3f10532f900f48be8c93c5acc90ed35c56d325c
en3m3 <mof20002@byui.edu>
2020-10-29 22:27:03 -0600

    lazyloader

---
+++ lesson7/js/lazy.js
@@ -0,0 +1,32 @@
+"using strict"
+
+const imageNodeList = document.querySelectorAll("img[data-src]");
+
+function preloadImage(img) {
+    src = img.getAttribute("data-src");
+    if(!src) {
+        return;
+    }
+    img.src = src;
+}
+
+const options = {
+    threshold: 0.5
+};
+
+
+const imgObserver = new IntersectionObserver ((items, imgObserver) => {
+    items.forEach(item => {
+        if(!item.isIntersecting) {
+            return;
+        } else {
+            preloadImage(item.target);
+            imgObserver.unobserve(item.target);
+        }
+    });
+}, options);
+
+
+imageNodeList.forEach(image => {
+    imgObserver.observe(image);
+});
---
+++ lesson7/js/script.js
@@ -0,0 +1,31 @@
+const navtoggle = document.querySelector('#mobile-toggle');
+const nav = document.querySelector('nav');
+
+var months = ["Janurary", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November"];
+let modified = new Date();
+let formattedDate = `${months[modified.getMonth()]} ${modified.getDate()}, ${modified.getFullYear()}`;
+document.getElementById("modified").innerHTML = formattedDate;
+
+navtoggle.addEventListener("click", function () {
+    nav.classList.toggle("nav-open");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    if (document.body.classList.contains("preston")) {
+        let notice = document.querySelector("#friday-notice");
+        let today = new Date();
+        let weekDay = new Date().getDay();
+        if (weekDay === 5) {
+            notice.classList.remove("hide");
+        }
+    }
+});
+
+WebFont.load({
+    google: {
+        families: [
+            'Rubik',
+            'Montserrat',
+        ]
+    }
+});
