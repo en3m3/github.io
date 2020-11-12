1d2b6c0c1acd51145b9a0af45b3a0521959f75d0
en3m3 <mof20002@byui.edu>
2020-11-12 16:07:34 -0700

    town object added

--- lesson9/js/townData.js
+++ lesson9/js/townData.js
@@ -1,14 +1,91 @@
 const requestURL = 'towndata.json';
 
+function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+  }
+
+function Town(rainfall,
+              population,
+              events,
+              motto,
+              name,
+              photo,
+              founded
+              ) {
+    this.averageRainFall = population;
+    this.events = events;
+    this.motto = motto;
+    this.name = name;
+    this.photo = photo;
+    this.founded = founded
+
+}
+
+function getList(townEvents) {
+    var events = [];
+    townEvents.forEach(element => {
+        events.append(towns[i].element);
+    });
+    return eventList;
+}
+
+var siteTowns = [];
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    const towns = jsonObject['prophets'];      
+    const towns = jsonObject['towns'];      
     console.table(jsonObject);  
-    // for (let i = 0; i < prophets.length; i++ ) {
-    //     let card = document.createElement('section');
+    townArray = {};
+    for (let i = 0; i < towns.length; i++ ) {
+        if(towns[i].name == "Fish Haven") {
+            var eventList = getList(towns[i].events);
+
+            var fishHaven = Town(
+                                towns[i].averageRainfall,
+                                towns[i].currentPopulation,
+                                eventList,
+                                towns[i].motto,
+                                towns[i].name,                                
+                                towns[i].photo,
+                                towns[i].yearFounded,                                  
+                                )
+            townArray["fishHaven"]= fishHaven;
+        } else if(towns[i].name == "Preston") {
+            var eventList = getList(towns[i].events);
+
+            var preston = Town(
+                                towns[i].averageRainfall,
+                                towns[i].currentPopulation,
+                                eventList,
+                                towns[i].motto,
+                                towns[i].name,                                
+                                towns[i].photo,
+                                towns[i].yearFounded,                                  
+                                )            
+            townArray["preston"]= preston;
+        } else if(towns[i].name == "Soda Springs") {
+            var eventList = getList(towns[i].events);
+
+            var sodaSprings = Town(
+                                towns[i].averageRainfall,
+                                towns[i].currentPopulation,
+                                eventList,
+                                towns[i].motto,
+                                towns[i].name,                                
+                                towns[i].photo,
+                                towns[i].yearFounded,                                  
+                                )            
+            townArray["sodaSprings"]= sodaSprings;
+        }   
+      }
+  });
+
+      //     let card = document.createElement('section');
     //     let h2 = document.createElement('h2');
     //     let birthday = document.createElement('p');
     //     let birthplace = document.createElement('p');
@@ -24,9 +101,3 @@ fetch(requestURL)
     //     card.appendChild(image); 
     //     image.setAttribute('src', prophets[i].imageurl);
     //     document.querySelector('.cards').appendChild(card);
-
-        
-    
-    //   }
-
-  });
