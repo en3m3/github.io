2d06a2cb69c7f31287fd8e8f846c012435c8b8bf
en3m3 <mof20002@byui.edu>
2020-11-12 20:20:29 -0700

    styling

--- lesson9/js/townData.js
+++ lesson9/js/townData.js
@@ -55,25 +55,6 @@ function buildTownStructure(containerClass, townObject){
     city.appendChild(cityRainfall);    
 }
 
-
-    //     let card = document.createElement('section');
-    //     let h2 = document.createElement('h2');
-    //     let birthday = document.createElement('p');
-    //     let birthplace = document.createElement('p');
-    //     let image = document.createElement('img');
-    //     h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-    //     birthday.textContent = "Date of Birth: "+ prophets[i].birthdate;
-    //     birthplace.textContent = "Place of Birth: "+ prophets[i].birthplace;
-
-    //     card.appendChild(h2);
-    //     card.appendChild(birthday);
-    //     card.appendChild(birthplace);
-    //     card.appendChild(image); 
-    //     image.setAttribute('src', prophets[i].imageurl);
-    //     document.querySelector('.cards').appendChild(card);
-
-
-
 function getList(townEvents) {
     var events = [];
     townEvents.forEach(element => {
