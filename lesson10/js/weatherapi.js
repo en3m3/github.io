914ae631cdcc574d32e1e176642050efe0282263
en3m3 <mof20002@byui.edu>
2020-11-17 00:24:15 -0700

    fixed js

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -3,14 +3,16 @@
 /* Soda Springs 5607916 */
 /* Fish Haven: 5585010  */
 
-if(document.body.classList.contains("preston")) {
-    const requestURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=ee95bdb22401d075cd5b22ae078a154f";
-} else if(document.body.classList.contains("sodaSprings")) {
-    const requestURL = "https://api.openweathermap.org/data/2.5/weather?id=5607916&appid=ee95bdb22401d075cd5b22ae078a154f";
-} else if(document.body.classList.contains("fishHaven")) {
-    const requestURL = "https://api.openweathermap.org/data/2.5/weather?id=5585010&appid=ee95bdb22401d075cd5b22ae078a154f";
+const bodyClasses = document.body.classList;
+console.log(bodyClasses);
+if(bodyClasses.contains("preston")) {
+    var requestURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=ee95bdb22401d075cd5b22ae078a154f";
+} else if(bodyClasses.contains("sodaSprings")) {
+    var requestURL = "https://api.openweathermap.org/data/2.5/weather?id=5607916&appid=ee95bdb22401d075cd5b22ae078a154f";
+} else if(bodyClasses.contains("fishHaven")) {
+    var requestURL = "https://api.openweathermap.org/data/2.5/weather?id=5585010&appid=ee95bdb22401d075cd5b22ae078a154f";
 }
-
+console.log(requestURL);
 fetch(requestURL)
     .then(function (response) {
         return response.json();
