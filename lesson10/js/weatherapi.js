ebf70ed19ee3b43a1921b66f01cc0ea880650819
en3m3 <mof20002@byui.edu>
2020-11-17 22:06:57 -0700

    more

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -4,22 +4,59 @@
 /* Fish Haven: 5585010  */
 
 const bodyClasses = document.body.classList;
-console.log(bodyClasses);
-if(bodyClasses.contains("preston") || bodyClasses.contains("apiTest")) {
+
+
+if(bodyClasses.contains("apiTest")) {
+    var page = "apiTest";
     var requestURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=ee95bdb22401d075cd5b22ae078a154f";
-} else if(bodyClasses.contains("sodaSprings")) {
+}else if(bodyClasses.contains("preston") ) { 
+    var page = "preston";
+    var requestURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=ee95bdb22401d075cd5b22ae078a154f";
+}else if(bodyClasses.contains("sodaSprings")) {
+    var page = "sodaSprings";
     var requestURL = "https://api.openweathermap.org/data/2.5/weather?id=5607916&appid=ee95bdb22401d075cd5b22ae078a154f";
 } else if(bodyClasses.contains("fishHaven")) {
+    var page = "fishHaven";
     var requestURL = "https://api.openweathermap.org/data/2.5/weather?id=5585010&appid=ee95bdb22401d075cd5b22ae078a154f";
 }
-console.log(requestURL);
+
 fetch(requestURL)
     .then(function (response) {
         return response.json();
     })
     .then((jsObject) => {
         console.log(jsObject);
-        document.getElementById('current-temp').textContent = jsObject.main.temp;    
-        document.getElementById('icon').src = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-        document.getElementById('icon').alt = jsObject.weather[0].description;
+        switch(page){
+            case "apiTest":
+                document.getElementById('current-temp').textContent = jsObject.main.temp;    
+                document.getElementById('icon').src = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+                document.getElementById('icon').alt = jsObject.weather[0].description;
+                break;
+            default:
+                document.getElementById('current').textContent = jsObject.weather[0].description;    
+                document.getElementById('icon').src = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+                document.getElementById('icon').alt = jsObject.weather[0].description;                
+                document.getElementById('temp').textContent = jsObject.main.temp;
+                document.getElementById('humidity').textContent = jsObject.main.humidity;
+                document.getElementById('wind-speed').textContent = jsObject.wind.speed;
+        }
     });
+/*
+    feels_like: 268.21
+humidity: 92
+pressure: 1025
+temp: 271.84
+temp_max: 274.26
+temp_min: 269.26
+__proto__: Object
+name: "Preston"
+sys: {type: 1, id: 4701, country: "US", sunrise: 1605622860, sunset: 1605657871}
+timezone: -25200
+visibility: 4828
+weather: [{…}]
+wind:
+deg: 74
+speed: 1.88
+__proto__: Object
+__proto__: Object
+*/
